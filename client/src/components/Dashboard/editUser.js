4e7819f2731f96f4e881079a841206fb1a9890c2
Dylan Crowley <dcrowdev1025@gmail.com>
2022-12-12 18:42:58 -0500

    basic dashboard pages layouts

--- client/src/components/Dashboard/editUser.js
+++ client/src/components/Dashboard/editUser.js
@@ -1,10 +1,63 @@
-import React from "react";
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
 
-function EditUser({handlePageChange}) {
-    return(
-        <h1>Edit User</h1>
-    )
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+function EditUser() {
+  return (
+    <Box>
+        <Typography sx={{ mt: 4, mb: 2 }} variant="h3" component="div">
+                        Account Information
+                    </Typography>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell>Calories</TableCell>
+            <TableCell>Fat&nbsp;(g)</TableCell>
+            <TableCell>Carbs&nbsp;(g)</TableCell>
+            <TableCell>Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell>{row.calories}</TableCell>
+              <TableCell>{row.fat}</TableCell>
+              <TableCell>{row.carbs}</TableCell>
+              <TableCell>{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </Box>
+  );
 }
 
 export default EditUser;
--- client/src/components/Dashboard/viewPackages.js
+++ client/src/components/Dashboard/viewPackages.js
@@ -1,10 +1,71 @@
-import React from "react";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import FolderIcon from '@mui/icons-material/Folder';
+import DeleteIcon from '@mui/icons-material/Delete';
 
+function generate(element) {
+    return [0, 1, 2].map((value) =>
+        React.cloneElement(element, {
+            key: value,
+        }),
+    );
+}
 
-function ViewPackages({handlePageChange}) {
-    return(
-        <h1>ViewPackages</h1>
-    )
+const Demo = styled('div')(({ theme }) => ({
+    backgroundColor: theme.palette.background.paper,
+}));
+
+function ViewPackages() {
+    const [dense, setDense] = React.useState(false);
+    const [secondary, setSecondary] = React.useState(false);
+
+    return (
+        <Box>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                    <Typography sx={{ mt: 4, mb: 2 }} variant="h3" component="div">
+                        My Packages
+                    </Typography>
+                    <Demo>
+                        <List dense={dense}>
+                            {generate(
+                                <ListItem
+                                    secondaryAction={
+                                        <IconButton edge="end" aria-label="delete">
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    }
+                                >
+                                    <ListItemAvatar>
+                                        <Avatar>
+                                            <FolderIcon />
+                                        </Avatar>
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary="Single-line item"
+                                        secondary={secondary ? 'Secondary text' : null}
+                                    />
+                                </ListItem>,
+                            )}
+                        </List>
+                    </Demo>
+                </Grid>
+            </Grid>
+        </Box>
+    );
 }
 
 export default ViewPackages;
