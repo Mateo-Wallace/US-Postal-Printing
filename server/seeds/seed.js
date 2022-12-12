479c473de716bd1bf909922a8c2c9fa53c313ed8
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-12-12 13:47:48 -0500

    seeds setup

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,34 +1,50 @@
-const db = require('../config/connection');
-const { User, Package, Order } = require('../models');
-const userSeeds = require('./userSeeds.json');
-const packageSeeds = require('./packageSeeds.json');
-const orderSeeds = require('./orderSeeds.json');
+const db = require("../config/connection");
+const { User, Package, Order } = require("../models");
+const userSeeds = require("./userSeeds.json");
+const packageSeeds = require("./packageSeeds.json");
+const orderSeeds = require("./orderSeeds.json");
 
-db.once('open', async () => {
+db.once("open", async () => {
   try {
     await Order.deleteMany({});
     await Package.deleteMany({});
     await User.deleteMany({});
 
+    // Creates Users
     await User.create(userSeeds);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-        const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
-        const user = await User.findOneAndUpdate(
-          { username: thoughtAuthor },
-          {
-            $addToSet: {
-              thoughts: _id,
-            },
-          }
-        );
-      }
+    // Creates packages and links to user
+    for (let i = 0; i < packageSeeds.length; i++) {
+      const { _id, username } = await Package.create(packageSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { username: username },
+        {
+          $addToSet: {
+            packages: _id,
+          },
+        }
+      );
+      console.log(user);
+    }
 
+    // Creates orders and links to user
+    for (let i = 0; i < orderSeeds.length; i++) {
+      const { _id, username } = await Package.create(orderSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { username: username },
+        {
+          $addToSet: {
+            orders: _id,
+          },
+        }
+      );
+      console.log(user);
+    }
   } catch (err) {
     console.error(err);
     process.exit(1);
   }
 
-  console.log('all done!');
+  console.log("all done!");
   process.exit(0);
 });
