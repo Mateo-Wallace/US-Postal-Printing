8d4f510e57052197e8fd357a47335083a347acb7
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-12-12 13:39:00 -0500

    set up seeds json files

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,12 +1,28 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Package, Order } = require('../models');
 const userSeeds = require('./userSeeds.json');
+const packageSeeds = require('./packageSeeds.json');
+const orderSeeds = require('./orderSeeds.json');
 
 db.once('open', async () => {
   try {
+    await Order.deleteMany({});
+    await Package.deleteMany({});
     await User.deleteMany({});
 
     await User.create(userSeeds);
+
+    for (let i = 0; i < thoughtSeeds.length; i++) {
+        const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
+        const user = await User.findOneAndUpdate(
+          { username: thoughtAuthor },
+          {
+            $addToSet: {
+              thoughts: _id,
+            },
+          }
+        );
+      }
 
   } catch (err) {
     console.error(err);
