1eaefdcd2a0884405eb7116852be948201bef56e
Dylan Crowley <dcrowdev1025@gmail.com>
2022-12-13 12:44:26 -0500

    added user query to edit user

--- client/src/components/Dashboard/editUser.js
+++ client/src/components/Dashboard/editUser.js
@@ -5,6 +5,8 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
 import EditIcon from '@mui/icons-material/Edit';
 import TextField from '@mui/material/TextField';
+import { CURRENT_USER } from "../../utils/queries";
+import { useQuery } from '@apollo/client';
 
 function EditUser() {
     const [expanded, setExpanded] = React.useState(false);
@@ -13,93 +15,99 @@ function EditUser() {
         setExpanded(isExpanded ? panel : false);
     };
 
+    const { loading, data } = useQuery(CURRENT_USER)
+
+    const userData = data?.me || {};
+
+    if (loading) {
+        return (
+            <h2>LOADING...</h2>
+        )
+    }
+
     return (
         <div>
             <Typography sx={{ mt: 4, mb: 2 }} variant="h3" component="div">
                 Edit Account
             </Typography>
-            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-                <AccordionSummary
-                    expandIcon={<EditIcon />}
-                    aria-controls="panel1bh-content"
-                    id="panel1bh-header"
-                >
-                    <Typography sx={{ width: '33%', flexShrink: 0, color: 'text.secondary' }}>
-                        Email
-                    </Typography>
-                    <Typography>test@test.com</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <TextField
-                        id="outlined-email-input"
-                        label="New Email"
-                        type="email"
-                        autoComplete="current-email"
-                    />
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-                <AccordionSummary
-                    expandIcon={<EditIcon />}
-                    aria-controls="panel2bh-content"
-                    id="panel2bh-header"
-                >
-                    <Typography sx={{ width: '33%', flexShrink: 0, color: 'text.secondary' }}>Username</Typography>
-                    <Typography>
-                        tester
-                    </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <TextField
-                        id="outlined-username-input"
-                        label="New Username"
-                        type="text"
-                        autoComplete="current-username"
-                    />
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-                <AccordionSummary
-                    expandIcon={<EditIcon />}
-                    aria-controls="panel3bh-content"
-                    id="panel3bh-header"
-                >
-                    <Typography sx={{ width: '33%', flexShrink: 0, color: 'text.secondary' }}>
-                        Password
-                    </Typography>
-                    <Typography>
-                        #########
-                    </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <TextField
-                        id="outlined-password-input"
-                        label="New Password"
-                        type="password"
-                        autoComplete="current-password"
-                    />
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
-                <AccordionSummary
-                    expandIcon={<EditIcon />}
-                    aria-controls="panel4bh-content"
-                    id="panel4bh-header"
-                >
-                    <Typography sx={{ width: '33%', flexShrink: 0, color: 'text.secondary' }}>Phone Number</Typography>
-                    <Typography>
-                        555-555-5555
-                    </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <TextField
-                        id="outlined-phonenumber-input"
-                        label="New Phone Number"
-                        type="text"
-                        autoComplete="current-phonenumber"
-                    />
-                </AccordionDetails>
-            </Accordion>
+                 <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                        <AccordionSummary
+                            expandIcon={<EditIcon />}
+                            aria-controls="panel1bh-content"
+                            id="panel1bh-header"
+                        >
+                            <Typography sx={{ width: '33%', flexShrink: 0, color: 'text.secondary' }}>
+                                Email
+                            </Typography>
+                            <Typography>{userData.email}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <TextField
+                                id="outlined-email-input"
+                                label="New Email"
+                                type="email"
+                                autoComplete="current-email" />
+                        </AccordionDetails>
+                    </Accordion>
+                        <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                            <AccordionSummary
+                                expandIcon={<EditIcon />}
+                                aria-controls="panel2bh-content"
+                                id="panel2bh-header"
+                            >
+                                <Typography sx={{ width: '33%', flexShrink: 0, color: 'text.secondary' }}>Username</Typography>
+                                <Typography>
+                                    {userData.username}
+                                </Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <TextField
+                                    id="outlined-username-input"
+                                    label="New Username"
+                                    type="text"
+                                    autoComplete="current-username" />
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                            <AccordionSummary
+                                expandIcon={<EditIcon />}
+                                aria-controls="panel3bh-content"
+                                id="panel3bh-header"
+                            >
+                                <Typography sx={{ width: '33%', flexShrink: 0, color: 'text.secondary' }}>
+                                    Password
+                                </Typography>
+                                <Typography>
+                                    #########
+                                </Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <TextField
+                                    id="outlined-password-input"
+                                    label="New Password"
+                                    type="password"
+                                    autoComplete="current-password" />
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+                            <AccordionSummary
+                                expandIcon={<EditIcon />}
+                                aria-controls="panel4bh-content"
+                                id="panel4bh-header"
+                            >
+                                <Typography sx={{ width: '33%', flexShrink: 0, color: 'text.secondary' }}>Phone Number</Typography>
+                                <Typography>
+                                    555-555-5555
+                                </Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <TextField
+                                    id="outlined-phonenumber-input"
+                                    label="New Phone Number"
+                                    type="text"
+                                    autoComplete="current-phonenumber" />
+                            </AccordionDetails>
+                        </Accordion>
         </div>
     );
 }
