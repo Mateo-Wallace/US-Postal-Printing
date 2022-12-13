c6b2775b289c7e97ff355869f458742b2995122a
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-12-12 20:36:33 -0500

    merge fix

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('@apollo/server');
+const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
