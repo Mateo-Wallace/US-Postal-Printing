d90ed1de769fc698fee6c4a588d5437bc46f89ac
Bfunk54 <ben_fein@icloud.com>
2022-12-13 17:32:16 -0500

    finished home page layout

--- client/src/App.js
+++ client/src/App.js
@@ -15,12 +15,14 @@ import Landing from './pages/Landing';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
 
 /// IMPORT COMPONENTS ///
 import ProtectRoute from './components/ProtectRoute';
 
 import './App.css';
 import Nav from './components/Header';
+
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -68,9 +70,10 @@ function App() {
       <ThemeProvider theme={theme}>
       <Nav></Nav>
         <Routes>
-          <Route path='/' element={<Landing />} />
+          <Route path='/' element={<Home />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/products&services' element={<Login />} />
           <Route
             path='dashboard/:userId'
             element={Auth.loggedIn() ? <Dashboard /> : <ProtectRoute />}
