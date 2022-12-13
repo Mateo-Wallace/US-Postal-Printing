047fd688351a653b801d1686c4175be964c77f7b
Bfunk54 <ben_fein@icloud.com>
2022-12-12 20:15:15 -0500

    fixing menu bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,19 +8,23 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import Hamburger from 'hamburger-react';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function Nav() {
+  const [isOpen, setOpen] = React.useState(false);
+  const [isClose, setClose] = React.useState(true);
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
+  const closeandSet = () => {
+    setAnchorElUser(null);
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
@@ -35,6 +39,11 @@ function Nav() {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+
+  // const handleOpenMenu = () => {
+  //   setOpen(true);
+  // };
+  
 
   return (
     <AppBar position="static">
@@ -128,8 +137,8 @@ function Nav() {
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="" />
+              <IconButton onClick={handleOpenUserMenu} >
+              <Hamburger toggled={isOpen} toggle={setOpen} rounded/>
               </IconButton>
             </Tooltip>
             <Menu
@@ -149,7 +158,7 @@ function Nav() {
               onClose={handleCloseUserMenu}
             >
               {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <MenuItem key={setting} onClick=[{handleCloseUserMenu}, {setOpen}]>
                   <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
               ))}
