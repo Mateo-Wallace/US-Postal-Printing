75dcbc80e495657b3741415b5feae1b81238d3f7
Bfunk54 <ben_fein@icloud.com>
2022-12-14 13:26:51 -0500

    added map to homepage with clickable link

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import logo from '..';
+import Map from '../images/US-Postal-Printing-Map.png';
+
 
 
 const Home = () => {
@@ -22,12 +23,12 @@ const Home = () => {
         },
       }}
     />
-    <Container sx={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center'}} fixed>
+    <Container sx={{margin: 0,  display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center'}} fixed>
       <Box
       sx={{
         margin: 1,
-        width: 400,
-        height: 300,
+        width: 550,
+        height: 400,
         backgroundColor: 'primary.dark',
         '&:hover': {
           backgroundColor: 'primary.main',
@@ -45,8 +46,8 @@ const Home = () => {
      <Box
       sx={{
         margin: 1,
-        width: 400,
-        height: 300,
+        width: 550,
+        height: 400,
         backgroundColor: 'primary.dark',
         '&:hover': {
           backgroundColor: 'primary.main',
@@ -57,6 +58,15 @@ const Home = () => {
     <Typography sx={{margin: 1, textAlign: 'center', color: 'white'}} variant="h4" gutterBottom>
     Location & Hours
   </Typography>
+  <a href="https://www.google.com/maps/place/Postal+%26+Printing/@26.7077343,-80.2237872,16z/data=!4m5!3m4!1s0x88d92f8cbb645c6b:0xe6fd7a8b86fec5e0!8m2!3d26.70705!4d-80.2191157" target="_blank" rel="noreferrer">
+  <img
+  width="325px"
+  height="210px"
+  frameBorder="0" style={{border: 0, margin: '8px'}}
+  src={Map}
+  href= 'https://surveyjs.io/form-library/examples/nps-question/reactjs'>
+</img>
+</a>
   </Box>
     <Box
       sx={{
