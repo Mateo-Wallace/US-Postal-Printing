ae6f7f6c5cc5eaaa9c5f2820a936b5f72bfb73f1
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-12-14 14:09:11 -0500

    finished contact form UI

--- client/src/pages/ContactUs.js
+++ client/src/pages/ContactUs.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-
+import React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const ContactUs = () => {
   return (
-    <div style={{display: 'flex'}}>
-     <Typography sx={{margin: 1, textAlign: 'center', color: 'black'}} variant="h4" gutterBottom>
-        Contact Us
-        </Typography>
+    <div>
+      <Card style={{ maxWidth: 450, margin: "0 auto", padding: "20px 5px" }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5">Contact Us</Typography>
+          <form>
+            <Grid container spacing={1}>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  label="First Name"
+                  placeholder="Enter first name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  label="Last Name"
+                  placeholder="Enter last name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField
+                  type="email"
+                  label="Email"
+                  placeholder="Enter email"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField
+                  label="Message"
+                  multiline
+                  rows={4}
+                  placeholder="Type your message here"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <Button type="submit" variant="contained">
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </CardContent>
+      </Card>
     </div>
-  )
+  );
 };
 
 export default ContactUs;
