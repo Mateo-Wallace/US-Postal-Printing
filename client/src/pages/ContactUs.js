4995b3eb991395ffdbe467d76fd8c8236bbb8251
Bfunk54 <ben_fein@icloud.com>
2022-12-14 12:22:08 -0500

    created contact us js file

---
+++ client/src/pages/ContactUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+const Home = () => {
+  return (
+    <div style={{display: 'flex'}}>
+     <Typography sx={{margin: 1, textAlign: 'center', color: 'white'}} variant="h4" gutterBottom>
+        Contact Us
+        </Typography>
+    </div>
+  )
+};
+
+export default Home;
