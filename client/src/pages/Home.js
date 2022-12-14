272ccb071f6e9e3b0eba778530bf76931b138d44
Bfunk54 <ben_fein@icloud.com>
2022-12-14 17:08:33 -0500

    finished a bunch of styling on the location and hours for the homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Map from '../images/US-Postal-Printing-Map.png';
+import Grid from '@mui/material/Grid';
+import Card  from '@mui/material/Paper';
 
 
 
@@ -43,38 +44,76 @@ const Home = () => {
     (at a glance)
   </Typography>
   </Box>
-     <Box
+     <Box 
       sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
         margin: 1,
         width: 550,
         height: 400,
         backgroundColor: 'primary.dark',
-        // '&:hover': {
-        //   backgroundColor: 'primary.main',
-        //   opacity: [0.9, 0.8, 0.7],
-        // },
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
       }}
     >
     <Typography sx={{margin: 1, textAlign: 'center', color: 'white'}} variant="h4" gutterBottom>
     Location & Hours
   </Typography>
   <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3564.081841247327!2d-80.1523419!3d26.7098323!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9290c5efab345%3A0x214ddf6757f47116!2sUS%20Postal%20%26%20Printing!5e0!3m2!1sen!2sus!4v1671043314933!5m2!1sen!2sus" 
-  width="400" 
-  height="300" 
-  style={{border:0}} 
+  width= "500"
+  height= "300"
+  style={{border:0, margin: '8px', borderRadius: '25px'}} 
   allowFullScreen="" 
   loading="lazy" 
   referrerPolicy="no-referrer-when-downgrade">
-
   </iframe>
-  {/* <img
-  width="325px"
-  height="210px"
-  frameBorder="0" style={{border: 0, margin: '8px'}}
-  src={Map}
-  href= 'https://surveyjs.io/form-library/examples/nps-question/reactjs'>
-</img> */}
-  </Box>
+  <Card sx={{fontSize: '16px', margin: 1}}>
+  <Grid sx={{margin: 1}} item>
+                <Box borderBottom={1}>STORE HOURS</Box>
+                <Grid container>
+                  <Grid item px={1}>
+                    <Box>MON</Box>
+                    <Box>9:00 AM</Box>
+                    <Box>6:00 PM</Box>
+                  </Grid>
+                  <Grid item px={1}>
+                    <Box>TUE</Box>
+                    <Box>9:00 AM</Box>
+                    <Box>6:00 PM</Box>
+                  </Grid>
+                  <Grid item px={1}>
+                    <Box>WED</Box>
+                    <Box>9:00 AM</Box>
+                    <Box>6:00 PM</Box>
+                  </Grid>
+                  <Grid item px={1}>
+                    <Box>THU</Box>
+                    <Box>9:00 AM</Box>
+                    <Box>6:00 PM</Box>
+                  </Grid>
+                  <Grid item px={1}>
+                    <Box>FRI</Box>
+                    <Box>9:00 AM</Box>
+                    <Box>6:00 PM</Box>
+                  </Grid>
+                  <Grid item px={1}>
+                    <Box>SAT</Box>
+                    <Box>10:00 AM</Box>
+                    <Box>2:00 PM</Box>
+                  </Grid>
+                  <Grid item px={1}>
+                    <Box>SUN</Box>
+                    <Box>Closed</Box>
+                  </Grid>
+                </Grid>
+              </Grid>
+              </Card>
+        </Box>
+        
+
     <Box
       sx={{
         margin: 1,
