0d3e1bed2a56554156b9f34ee69e9c44ca2551ec
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-12-14 11:22:34 -0500

    refactored tracking page to look like sign in

--- client/src/pages/Tracking.js
+++ client/src/pages/Tracking.js
@@ -1,32 +1,141 @@
-import React from "react";
-import { Model, StylesManager } from "survey-core";
-import { Survey } from "survey-react-ui";
-import "survey-core/defaultV2.css";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import { json } from "./OrderData";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
 
-StylesManager.applyTheme("defaultV2");
+import Auth from "../utils/auth";
 
-function onValueChanged(_, options) {
-  console.log("New value: " + options.value);
-}
+// material UI imports
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Link2 from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+// end material UI imports
 
-function onComplete(survey) {
-  console.log("Survey complete! Results: " + JSON.stringify(survey.data));
-}
+const theme = createTheme();
+// end material UI function
 
-const Tracking = () => {
-    const model = new Model(json);
-    return (
-      <div className="container">
-        <h1>SurveyJS Library / Runner</h1>
-        <Survey
-          model={model}
-          onComplete={onComplete}
-          onValueChanged={onValueChanged}
-        />
-      </div>
-    );
-}
+const Tracking = (props) => {
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
 
-export default Tracking
+  /// HANDLE CHANGE ///
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  /// FORM SUBMISSION ///
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      console.log(data)
+    } catch (error) {
+      console.log(error);
+    }
+
+    setFormState({
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <main>
+      {data ? (
+        <p>
+          Successfully logged in! You may now head{" "}
+          <Link to="/">back to the hompage.</Link>
+        </p>
+      ) : (
+        <ThemeProvider theme={theme}>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 8,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{ mt: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container justifyContent="flex-end">
+                  <Grid item>
+                    <Link2 href="/signup" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link2>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+          </Container>
+        </ThemeProvider>
+      )}
+      {error && <div>{error.message}</div>}
+    </main>
+  );
+};
+
+export default Tracking;
