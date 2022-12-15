312781af814f24878382a850ef45c73bbf218fe0
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-12-15 14:32:06 -0500

    added conditionally render items on orders page

--- client/src/pages/OrderData.js
+++ client/src/pages/OrderData.js
@@ -28,7 +28,8 @@ export const json = {
         ]
       }, {
         type: "panel",
-        title: "Expression Example Panel",
+        title: "Business Cards",
+        visibleIf: "{product}='Business Cards'",
         innerIndent: 1,
         elements: [
           {
@@ -79,7 +80,7 @@ export const json = {
                 title: "Total Item Cost:",
                 expression: "{panel.cost} * {panel.quantity}",
                 displayStyle: "currency",
-                currency: "EUR",
+                currency: "DOLLAR",
                 startWithNewLine: false
               }
             ],
@@ -103,7 +104,595 @@ export const json = {
                 title: "Total Cost:",
                 expression: "sumInArray({items}, 'total'",
                 displayStyle: "currency",
-                currency: "EUR",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ]
+          }
+        ]
+      }, {
+        type: "panel",
+        title: "Printing Services",
+        visibleIf: "{product}='Printing Services'",
+        innerIndent: 1,
+        elements: [
+          {
+            type: "paneldynamic",
+            name: "items",
+            title: "Items",
+            keyName: "name",
+            showQuestionNumbers: "none",
+            templateTitle: "item #{panelIndex}",
+            templateElements: [
+              {
+                type: "text",
+                name: "name",
+                title: "Name:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "cost",
+                inputType: "number",
+                title: "Item Cost:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "vendor",
+                title: "Vendor:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "quantity",
+                inputType: "number",
+                title: "Quantity:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "link",
+                title: "Link:",
+                isRequired: true
+              },
+              {
+                type: "expression",
+                name: "total",
+                title: "Total Item Cost:",
+                expression: "{panel.cost} * {panel.quantity}",
+                displayStyle: "currency",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ],
+            minPanelCount: 1,
+            panelAddText: "Add another  item",
+            panelRemoveText: "Remove item"
+          }, 
+          {
+            type: "panel",
+            title: "Totals",
+            elements: [
+              {
+                type: "expression",
+                name: "totalQuantity",
+                title: "Total  Quantity:",
+                expression: "sumInArray({items}, 'quantity'"
+              },
+              {
+                type: "expression",
+                name: "totalCost",
+                title: "Total Cost:",
+                expression: "sumInArray({items}, 'total'",
+                displayStyle: "currency",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ]
+          }
+        ]
+      }, {
+        type: "panel",
+        title: "Copying Services",
+        visibleIf: "{product}='Copying Services'",
+        innerIndent: 1,
+        elements: [
+          {
+            type: "paneldynamic",
+            name: "items",
+            title: "Items",
+            keyName: "name",
+            showQuestionNumbers: "none",
+            templateTitle: "item #{panelIndex}",
+            templateElements: [
+              {
+                type: "text",
+                name: "name",
+                title: "Name:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "cost",
+                inputType: "number",
+                title: "Item Cost:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "vendor",
+                title: "Vendor:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "quantity",
+                inputType: "number",
+                title: "Quantity:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "link",
+                title: "Link:",
+                isRequired: true
+              },
+              {
+                type: "expression",
+                name: "total",
+                title: "Total Item Cost:",
+                expression: "{panel.cost} * {panel.quantity}",
+                displayStyle: "currency",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ],
+            minPanelCount: 1,
+            panelAddText: "Add another  item",
+            panelRemoveText: "Remove item"
+          }, 
+          {
+            type: "panel",
+            title: "Totals",
+            elements: [
+              {
+                type: "expression",
+                name: "totalQuantity",
+                title: "Total  Quantity:",
+                expression: "sumInArray({items}, 'quantity'"
+              },
+              {
+                type: "expression",
+                name: "totalCost",
+                title: "Total Cost:",
+                expression: "sumInArray({items}, 'total'",
+                displayStyle: "currency",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ]
+          }
+        ]
+      },{
+        type: "panel",
+        title: "Passport Photos",
+        visibleIf: "{product}='Passport Photos'",
+        innerIndent: 1,
+        elements: [
+          {
+            type: "paneldynamic",
+            name: "items",
+            title: "Items",
+            keyName: "name",
+            showQuestionNumbers: "none",
+            templateTitle: "item #{panelIndex}",
+            templateElements: [
+              {
+                type: "text",
+                name: "name",
+                title: "Name:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "cost",
+                inputType: "number",
+                title: "Item Cost:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "vendor",
+                title: "Vendor:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "quantity",
+                inputType: "number",
+                title: "Quantity:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "link",
+                title: "Link:",
+                isRequired: true
+              },
+              {
+                type: "expression",
+                name: "total",
+                title: "Total Item Cost:",
+                expression: "{panel.cost} * {panel.quantity}",
+                displayStyle: "currency",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ],
+            minPanelCount: 1,
+            panelAddText: "Add another  item",
+            panelRemoveText: "Remove item"
+          }, 
+          {
+            type: "panel",
+            title: "Totals",
+            elements: [
+              {
+                type: "expression",
+                name: "totalQuantity",
+                title: "Total  Quantity:",
+                expression: "sumInArray({items}, 'quantity'"
+              },
+              {
+                type: "expression",
+                name: "totalCost",
+                title: "Total Cost:",
+                expression: "sumInArray({items}, 'total'",
+                displayStyle: "currency",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ]
+          }
+        ]
+      },{
+        type: "panel",
+        title: "Notary Services",
+        visibleIf: "{product}='Notary Services'",
+        innerIndent: 1,
+        elements: [
+          {
+            type: "paneldynamic",
+            name: "items",
+            title: "Items",
+            keyName: "name",
+            showQuestionNumbers: "none",
+            templateTitle: "item #{panelIndex}",
+            templateElements: [
+              {
+                type: "text",
+                name: "name",
+                title: "Name:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "cost",
+                inputType: "number",
+                title: "Item Cost:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "vendor",
+                title: "Vendor:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "quantity",
+                inputType: "number",
+                title: "Quantity:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "link",
+                title: "Link:",
+                isRequired: true
+              },
+              {
+                type: "expression",
+                name: "total",
+                title: "Total Item Cost:",
+                expression: "{panel.cost} * {panel.quantity}",
+                displayStyle: "currency",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ],
+            minPanelCount: 1,
+            panelAddText: "Add another  item",
+            panelRemoveText: "Remove item"
+          }, 
+          {
+            type: "panel",
+            title: "Totals",
+            elements: [
+              {
+                type: "expression",
+                name: "totalQuantity",
+                title: "Total  Quantity:",
+                expression: "sumInArray({items}, 'quantity'"
+              },
+              {
+                type: "expression",
+                name: "totalCost",
+                title: "Total Cost:",
+                expression: "sumInArray({items}, 'total'",
+                displayStyle: "currency",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ]
+          }
+        ]
+      },{
+        type: "panel",
+        title: "Fax Services",
+        visibleIf: "{product}='Fax Services'",
+        innerIndent: 1,
+        elements: [
+          {
+            type: "paneldynamic",
+            name: "items",
+            title: "Items",
+            keyName: "name",
+            showQuestionNumbers: "none",
+            templateTitle: "item #{panelIndex}",
+            templateElements: [
+              {
+                type: "text",
+                name: "name",
+                title: "Name:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "cost",
+                inputType: "number",
+                title: "Item Cost:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "vendor",
+                title: "Vendor:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "quantity",
+                inputType: "number",
+                title: "Quantity:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "link",
+                title: "Link:",
+                isRequired: true
+              },
+              {
+                type: "expression",
+                name: "total",
+                title: "Total Item Cost:",
+                expression: "{panel.cost} * {panel.quantity}",
+                displayStyle: "currency",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ],
+            minPanelCount: 1,
+            panelAddText: "Add another  item",
+            panelRemoveText: "Remove item"
+          }, 
+          {
+            type: "panel",
+            title: "Totals",
+            elements: [
+              {
+                type: "expression",
+                name: "totalQuantity",
+                title: "Total  Quantity:",
+                expression: "sumInArray({items}, 'quantity'"
+              },
+              {
+                type: "expression",
+                name: "totalCost",
+                title: "Total Cost:",
+                expression: "sumInArray({items}, 'total'",
+                displayStyle: "currency",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ]
+          }
+        ]
+      },{
+        type: "panel",
+        title: "Shipping Services",
+        visibleIf: "{product}='Shipping Services'",
+        innerIndent: 1,
+        elements: [
+          {
+            type: "paneldynamic",
+            name: "items",
+            title: "Items",
+            keyName: "name",
+            showQuestionNumbers: "none",
+            templateTitle: "item #{panelIndex}",
+            templateElements: [
+              {
+                type: "text",
+                name: "name",
+                title: "Name:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "cost",
+                inputType: "number",
+                title: "Item Cost:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "vendor",
+                title: "Vendor:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "quantity",
+                inputType: "number",
+                title: "Quantity:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "link",
+                title: "Link:",
+                isRequired: true
+              },
+              {
+                type: "expression",
+                name: "total",
+                title: "Total Item Cost:",
+                expression: "{panel.cost} * {panel.quantity}",
+                displayStyle: "currency",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ],
+            minPanelCount: 1,
+            panelAddText: "Add another  item",
+            panelRemoveText: "Remove item"
+          }, 
+          {
+            type: "panel",
+            title: "Totals",
+            elements: [
+              {
+                type: "expression",
+                name: "totalQuantity",
+                title: "Total  Quantity:",
+                expression: "sumInArray({items}, 'quantity'"
+              },
+              {
+                type: "expression",
+                name: "totalCost",
+                title: "Total Cost:",
+                expression: "sumInArray({items}, 'total'",
+                displayStyle: "currency",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ]
+          }
+        ]
+      },{
+        type: "panel",
+        title: "Other",
+        visibleIf: "{product}='Other'",
+        innerIndent: 1,
+        elements: [
+          {
+            type: "paneldynamic",
+            name: "items",
+            title: "Items",
+            keyName: "name",
+            showQuestionNumbers: "none",
+            templateTitle: "item #{panelIndex}",
+            templateElements: [
+              {
+                type: "text",
+                name: "name",
+                title: "Name:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "cost",
+                inputType: "number",
+                title: "Item Cost:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "vendor",
+                title: "Vendor:",
+                isRequired: true
+              },
+              {
+                type: "text",
+                name: "quantity",
+                inputType: "number",
+                title: "Quantity:",
+                isRequired: true,
+                startWithNewLine: false
+              },
+              {
+                type: "text",
+                name: "link",
+                title: "Link:",
+                isRequired: true
+              },
+              {
+                type: "expression",
+                name: "total",
+                title: "Total Item Cost:",
+                expression: "{panel.cost} * {panel.quantity}",
+                displayStyle: "currency",
+                currency: "DOLLAR",
+                startWithNewLine: false
+              }
+            ],
+            minPanelCount: 1,
+            panelAddText: "Add another  item",
+            panelRemoveText: "Remove item"
+          }, 
+          {
+            type: "panel",
+            title: "Totals",
+            elements: [
+              {
+                type: "expression",
+                name: "totalQuantity",
+                title: "Total  Quantity:",
+                expression: "sumInArray({items}, 'quantity'"
+              },
+              {
+                type: "expression",
+                name: "totalCost",
+                title: "Total Cost:",
+                expression: "sumInArray({items}, 'total'",
+                displayStyle: "currency",
+                currency: "DOLLAR",
                 startWithNewLine: false
               }
             ]
