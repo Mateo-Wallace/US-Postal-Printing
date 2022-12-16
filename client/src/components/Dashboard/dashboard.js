b5520f0b3deb8be4797b3ec4912f6c546173cd46
Dylan Crowley <dcrowdev1025@gmail.com>
2022-12-16 13:42:44 -0500

    basic inner dashboard styles

--- client/src/components/Dashboard/dashboard.js
+++ client/src/components/Dashboard/dashboard.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { CURRENT_USER } from "../../utils/queries";
 import { useQuery } from '@apollo/client';
+import Carousel from 'react-material-ui-carousel'
+import Item from './carouselitem'
+import carousel from '../../utils/carousel.json'
+
+
+
 
 function InnerDashboard() {
     const { loading, data } = useQuery(CURRENT_USER)
@@ -13,8 +19,17 @@ function InnerDashboard() {
         )
       }
 
-    return(
-        <h1>Hello {userData.username}</h1>
+
+    return (
+      <> 
+        <h2 style={{textAlign: 'center'}}>Hello</h2>
+        <h1 style={{textAlign: 'center', textDecoration: 'underline', marginBottom: '10%', fontFamily: 'Gill Sans, sans-serif'}}>{userData.username}!</h1>
+        <Carousel>
+            {
+                carousel.map( item => <Item key={item.id} item={item} /> )
+            }
+        </Carousel>
+        </>
     )
 }
 
