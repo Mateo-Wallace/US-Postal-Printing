0e9b351d397a4b51e725e3d5e6afa2d7ed5c2f85
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-12-17 05:05:12 -0500

    setup orderData to log user input. set up makeAnOrder function

--- client/src/pages/MakeAnOrder.js
+++ client/src/pages/MakeAnOrder.js
@@ -1,30 +1,40 @@
 import React from "react";
+import { useCallback } from "react";
 import { Model, StylesManager } from "survey-core";
 import { Survey } from "survey-react-ui";
 import "survey-core/defaultV2.css";
+import { useMutation } from "@apollo/client";
+import { ADD_PACKAGE } from "../utils/mutations";
 
 import { surveyJson } from "./OrderData";
 
 StylesManager.applyTheme("defaultV2");
 
-function onValueChanged(_, options) {
-  console.log("New value: " + options.value);
-}
-
-function onComplete(e) {
-  console.log("Survey complete! Results: " + JSON.stringify(e.data));
-}
-
 const MakeAnOrder = () => {
   const survey = new Model(surveyJson);
+  const [addPackage, { error, data }] = useMutation(ADD_PACKAGE);
+
+  const handleSaveOrder = useCallback(async (sender) => {
+    const results = JSON.stringify(sender.data);
+    console.log(results);
+
+    try {
+      const data = await addPackage({
+        variables: { trackingNum: 'p.trackingNumber', carrier: 'p.carrier' },
+      });
+      console.log(data);
+
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  survey.onComplete.add(handleSaveOrder);
+
   return (
     <div className="container">
       <h1>SurveyJS Library / Runner</h1>
-      <Survey
-        model={survey}
-        onComplete={onComplete}
-        onValueChanged={onValueChanged}
-      />
+      <Survey model={survey} />
     </div>
   );
 };
